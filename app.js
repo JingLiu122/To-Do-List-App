0633ab3c3a0836ff9cb6da722adb8be60195392f
Jing L <jingl@Jings-MacBook-Pro.local>
2019-09-30 10:04:55 -0400

    some updates

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const addData = () => {
   const data = document.getElementById("addListItem").value;
   let list = {
     todo: data,
-    status: '',
+    status: STATUS[0],
   };
   lists.push(list);
   document.getElementById("addListItem").value = "";
@@ -20,12 +20,17 @@ const addData = () => {
   init();
 }
 
+// remove data from the lists based on the clicked checkbox button
 const removeData = (ev) => {
   const id = ev.target.id;
   const index = parseInt(id.charAt(id.length-1))-1;
   lists.splice(index, 1);
   
   init();
+}
+
+const updateData = (value) => {
+ console.log(value);
 }
 
 // A listener function to display lists to the HTML DOM once user clicks a display button
@@ -39,7 +44,8 @@ function listsDisplayListener() {
 
     rowTemplate += '<div class="grid-row" id="row' + (i+1) + '">';
     columnTemplate += '<div class="grid-column" id="col' + (col+1) + '">' + list.todo + '</div>';  
-    columnTemplate += '<div class="grid-column" id="col' + (col+2) + '">' + list.status + '</div>';
+    columnTemplate += '<div class="grid-column" id="col' + (col+2) + '">' + list.status +
+    dropDownListOptionsTemplate(i+1) + '</div>';
     columnTemplate += '<div class="grid-column" id="col' + (col+3) + '">' + 
     '<input type="checkbox" id="delBtn' + (i+1) + '">' + '</div>';
     rowTemplate += columnTemplate;
@@ -47,7 +53,6 @@ function listsDisplayListener() {
   });
   document.getElementById("app").innerHTML = rowTemplate;
 }
-
 
 // this function is getting all the checkbox ids,
 // and then make an event listener to listen all checkbox clicks
@@ -65,6 +70,37 @@ function getCheckBoxIDs() {
   });
 }
 
+const dropDownListOptionsTemplate = (index) => {
+  const str = '<div class="popup" id="pop">' + '<button onclick="popup()">...</button>' + 
+  '<span class="popuptext" id="myPopup">' + '<select id="select-status">' +
+  '<option>---</option>' + '<option value="Started">Started</option>' + '<option value="Holding">Holding</option>' +
+  '<option value="Finished">Finished</option>' + '<option value="Incomplete">Incomplete</option>' + '</select>' +
+  '</span>' + '</div>';
+  return str;
+}
+
+// This function will return the selected value from the drop-down list
+const dropDownListOptions = (event) => {
+  // event.target.parentNode;
+  // remove event listener
+}
+
+const getSelectedValue = () => {
+  const selects = document.getElementById('select-status').value;
+  // let value = '';
+  // if(selects !== event.target.options[0].value){
+  //   value = selects;
+  // }
+  // return value;
+  console.log(selects);
+}
+
+// When the user clicks on div, open the popup
+function popup() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
 /*============================================================================================================
 An initial event for the browser to finish reading and loading all HTMLs into the DOM first, and then 
 perform the other events.
@@ -73,6 +109,35 @@ document.addEventListener('DOMContentLoaded', init);
 function init(){
   listsDisplayListener();
   document.getElementById("addBtn").addEventListener("click", addData);
-  document.getElementById("refreshBtn").addEventListener("click", listsDisplayListener);
+  // document.getElementById("refreshBtn").addEventListener("click", listsDisplayListener);
+  document.getElementById('').onchange = () => {dropDownListOptions(event)};
+  dropDownListOptions(event);
   getCheckBoxIDs();
 }
+
+
+
+
+
+
+
+
+
+
+
+// const tags = document.getElementsByClassName('popup');
+// const selectIDs = [];
+
+// for(let i = 0; i < tags.length; i++){
+//   selectIDs.push(tags[i].id);   
+// }
+
+// selectIDs.forEach((id, index) => {
+//   document.getElementById(id).addEventListener('click', () => {
+//     document.getElementById('select-status').onchange = () => {
+//       const selects = document.getElementById('select-status').value;
+//       console.log(id, index, selects);
+//     };
+//   });
+// });
+     
